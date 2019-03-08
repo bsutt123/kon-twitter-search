c51f97a0864bef6633a0d1c644834811c38ff97f
Brady Sutton <bsutt123@gmail.com>
2019-03-08 08:31:44 -0500

    create routes for 2 requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "tweets/search", to: "tweets#search"
+  get "tweets/user", to: "tweets#user"
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
